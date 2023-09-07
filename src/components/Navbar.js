e21192af66cc45d17cfc29ecbabf89d94bb23c8d
somboonnontaganok <somboonnontaganok@gmail.com>
2023-09-07 10:40:43 +0700

    add cart button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between item-center p-4">
-        {/* Left side */}
+      {/* Left side */}
       <div className="flex items-center">
         <div className="curor-pointer">
           <AiOutlineMenu size={30} />
@@ -12,19 +13,32 @@ const Navbar = () => {
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
           Best <span>Eats</span>
         </h1>
-        <div className="hidden lg:flex items-center bg-gray-200
-        rounded-full p-1 text-[14px]">
-            <p className="bg-black text-white rounded-full p-2">Delivery</p>
-            <p className="p-2">Pickup</p>
+        <div
+          className="hidden lg:flex items-center bg-gray-200
+        rounded-full p-1 text-[14px]"
+        >
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
         </div>
       </div>
 
       {/* Searh Input */}
-      <div className="bg-gray-200 rounded-full flex items-center px-2 
-      w-[200px] sm:w-[400px] lg:w-[500px]">
+      <div
+        className="bg-gray-200 rounded-full flex items-center px-2 
+      w-[200px] sm:w-[400px] lg:w-[500px]"
+      >
         <AiOutlineSearch size={25} />
-        <input className="bg-transparent p-2 focus:outline-none w-full" type="text" placeholder="Search foods"/>
+        <input
+          className="bg-transparent p-2 focus:outline-none w-full"
+          type="text"
+          placeholder="Search foods"
+        />
       </div>
+      {/* Cart button */}
+      <button className="bg-black text-white hidden md:flex items-center 
+      py-2 rounded-full ">
+        <BsFillCartFill size={20} className="mr-2"/> Cart
+      </button>
     </div>
   );
 };
