215d8b4f2c8f8e5cd4be38b0faf8821ec2714964
somboonnontaganok <somboonnontaganok@gmail.com>
2023-09-07 09:55:03 +0700

    Do not forget to install react-icons

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiOutlineMenu} from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <AiOutlineMenu />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
